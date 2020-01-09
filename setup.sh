12f5d357f1ff13eb195515497de80bef3943b35d
Hiroshi IKEGAMI <hiroshi.ikegami@satori.team>
2020-01-09 01:30:31 +0000

    cleanup

--- setup.sh
+++ setup.sh
@@ -23,35 +23,9 @@ fi
 mkdir -p ${current}/vim/bundle/
 mkdir -p  ~/.vimundo
 
-libs=('neobundle.vim' 'vimproc');
-for x in ${libs[@]}; do
-  if [ ! -d ${current}/vim/bundle/${x} ];then
-    git clone git://github.com/Shougo/${x} ${current}/vim/bundle/${x}
-  fi
-done;
 
 mkdir -p ${HOME}/.vim-backup && chmod 766 ${HOME}/.vim-backup
 mkdir -p ${HOME}/.vim-yankring
-
-vimproc_build() {
-  cd ${current}/vim/bundle/vimproc
-
-  local kernelname=$(uname -s)
-  local makefile_name=''
-  case ${kernelname} in
-    Linux | FreeBSD)
-      makefile_name=make_unix.mak
-      ;;
-    Darwin)
-      makefile_name=make_mac.mak
-      ;;
-  esac
-
-  make -f ${makefile_name} clean
-  make -f ${makefile_name}
-
-  cd ${current}
-}
 
 
 files=( 'vimrc' 'gvimrc' 'vim' );
