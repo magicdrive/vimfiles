3ba3f940a5ddca0dbe8dad9b43e22941d16543bd
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2020-03-06 07:59:16 +0000

    rakefile

--- vim/misc/template/ruby/base-rakefile.rb
+++ vim/misc/template/ruby/base-rakefile.rb
@@ -1,11 +1,34 @@
 # coding: utf-8
 
-task :default => [:spec]
-begin
-  require 'rspec/core/rake_task'
-  RSpec::Core::RakeTask.new(:spec) do |spec|
-    spec.pattern = 'spec/**/*_spec.rb'
-    spec.rspec_opts = ['-cfs']
+ProjectHome    = File.expand_path(File.dirname(__FILE__))
+ProjectName    = File.expand_path(File.dirname(__FILE__))
+DistinationDir = "#{ProjectHome}/target"
+SourceDir      = "#{ProjectHome}/src"
+LogDir         = "#{ProjectHome}/logs"
+
+
+task :default => [:build]
+
+desc "build #{ProjectName}"
+task :build do
+  sh "echo build"
+end
+
+desc "check working #{ProjectName}"
+task :check do
+  sh "echo check"
+end
+
+desc "create log and source directories"
+task :dirs do
+  sh %<mkdir -p #{SourceDir}/>, verbose: false
+  sh %<mkdir -p #{LogDir}/>, verbose: false
+end
+
+desc "monitoring #{ProjectName} sources."
+task watch: [:dirs] do
+  Dir.chdir("#{SourceDir}") do
+    sh %<reflex -v -g "#{SourceDir}/*.hoge" -- rake check filename={} | tee -a #{LogDir}/watch.log>
   end
-rescue LoadError => e
+
 end
